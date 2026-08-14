f8efd2540aa9458fc1823608d49742de2ff4e2b3
SeifESlimene <s.slimene19@gmail.com>
2026-08-14 14:11:25 +0100

    rename DeepTutor to Encore across the application

--- web/app/(workspace)/home/[[...sessionId]]/page.tsx
+++ web/app/(workspace)/home/[[...sessionId]]/page.tsx
@@ -1990,7 +1990,7 @@ export default function ChatPage() {
                 <div className="w-full max-w-[960px] flex items-center justify-center gap-4">
                   <img
                     src="/logo_black.png"
-                    alt="DeepTutor"
+                    alt="Encore"
                     width={40}
                     height={40}
                     className="h-10 w-10 select-none"
